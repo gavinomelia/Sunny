16517debadd50cfaa706290fe31c05d6c48e9752
Gavin OMelia <gavin@omelia.org>
2025-01-14 15:53:21 -0500

    Use ipapi.co to allow user to find forecast based on current IP address
    - Add a use my current location button in the locations view
    - Add a method to the locations controller to get current location
      longitude and latitude as well as city and state for the name
    - Alerts should now display properly after adding them to the layout
    - Close #5

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,6 @@
 class LocationsController < ApplicationController
+  require 'net/http'
+  require 'json'
   before_action :set_location, only: [ :show, :edit, :update, :destroy, :forecast ]
 
   def index
@@ -43,6 +45,37 @@ class LocationsController < ApplicationController
     @location = current_user.locations.find(params[:id])
     @location.destroy
     redirect_to locations_path, notice: "Location deleted successfully."
+  end
+
+  def current_location_forecast
+    url = URI.parse('https://ipapi.co/json')
+    response = Net::HTTP.get(url)
+    location_data = JSON.parse(response)
+
+    if location_data.present? && location_data["latitude"] && location_data["longitude"]
+      @latitude = location_data["latitude"]
+      @longitude = location_data["longitude"]
+      @city = location_data["city"]
+      @state = location_data["region"]
+
+      # Save the location for the current user
+      location = current_user.locations.build(
+        name: "#{@city}, #{@state}",
+        latitude: @latitude,
+        longitude: @longitude
+      )
+
+      if location.save
+        flash[:notice] = "Location successfully saved!"
+        redirect_to locations_path
+      else
+        flash[:alert] = "Unable to save the location. #{location.errors.full_messages.to_sentence}"
+        redirect_to locations_path
+      end
+    else
+      flash[:alert] = "Unable to determine your location. Please try again."
+      redirect_to locations_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-   resources :locations, only: [:index, :show, :new, :create, :destroy] do
+  resources :locations, only: [:index, :show, :new, :create, :destroy] do
     member do
       get :forecast
     end
   end
+
+  get "current_location_forecast", to: "locations#current_location_forecast", as: :current_location_forecast
+  get 'forecast', to: 'locations#forecast', as: :forecast
 
   get "/login", to: "sessions#new", as: :login
   post "/login", to: "sessions#create"
